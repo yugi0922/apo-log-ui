59cd00755c0723a1ffea15000f6406552f1589f9
DESKTOP-4HV9UQO\user <yugi-0922@outlook.jp>
2023-10-09 19:32:20 +0900

    デザイン1通り完成

--- src/components/pages/Quiz.tsx
+++ src/components/pages/Quiz.tsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Player } from "../models/interface";
 
-// Axiosのインスタンスを作成
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 const Quiz = () => {
   const [player, setPlayer] = useState<Player | null>(null);
-  const [hint, setHint] = useState<string>("");
+  const [hintsShown, setHintsShown] = useState<{ [key: string]: boolean }>({
+    team: false,
+    position: false,
+    size: false,
+  });
   const [answer, setAnswer] = useState<string>("");
   const [result, setResult] = useState<string>("");
 
@@ -30,26 +33,8 @@ const Quiz = () => {
     return <div className="text-center p-5">データ取得中...</div>;
   }
 
-  const showHint = (hintType: number) => {
-    switch (hintType) {
-      case 1:
-        setHint(
-          `|チーム|：${player.team}${player.team2 ? "-" + player.team2 : ""}`
-        );
-        break;
-      case 2:
-        setHint(
-          `|ポジション|：${player.position}${
-            player.position2 ? "/" + player.position2 : ""
-          }`
-        );
-        break;
-      case 3:
-        setHint(`|体重/身長|：${player.kg}/${player.meter}`);
-        break;
-      default:
-        break;
-    }
+  const showHint = (hintType: string) => {
+    setHintsShown((prev) => ({ ...prev, [hintType]: true }));
   };
 
   const checkAnswer = () => {
@@ -62,66 +47,114 @@ const Quiz = () => {
 
   return (
     <div className="bg-gray-50 flex flex-col items-center p-6 space-y-6">
-      <div className="flex flex-col items-center p-6 space-y-6">
-        <h1 className="text-xl font-bold">対象プレイヤー</h1>
-        <div className="flex w-full space-x-6">
-          {/* Statsエリア */}
-          <div className="flex-1 p-5 border rounded-md">
-            <h2 className="text-lg font-medium mb-4">Statsエリア</h2>
-            <div>GAME: {player.game}</div>
-            <div>GS: {player.gs}</div>
-            <div>MPG: {player.mpg}</div>
-            <div>PPG: {player.ppg}</div>
-            <div>RPG: {player.rpg}</div>
-            <div>APG: {player.apg}</div>
-            <div>SPG: {player.spg}</div>
-            <div>BPG: {player.bpg}</div>
-            <div>TURNOVER: {player.turnover}</div>
-            <div>PERCENT_FG: {player.percent_fg}%</div>
-            <div>PERCENT_3POINT: {player.percent_3point}%</div>
-            <div>PERCENT_FT: {player.percent_ft}%</div>
-          </div>
-
-          {/* ヒントゾーン */}
-          <div className="flex-1 p-5 border rounded-md">
-            <h2 className="text-lg font-medium mb-4">ヒントゾーン</h2>
-            <button
-              className="px-4 py-2 mr-2 bg-blue-500 text-white rounded-md"
-              onClick={() => showHint(1)}
-            >
-              ヒント1
-            </button>
-            <button
-              className="px-4 py-2 mr-2 bg-blue-500 text-white rounded-md"
-              onClick={() => showHint(2)}
-            >
-              ヒント2
-            </button>
-            <button
-              className="px-4 py-2 bg-blue-500 text-white rounded-md"
-              onClick={() => showHint(3)}
-            >
-              ヒント3
-            </button>
-            <div className="mt-4">{hint}</div>
+      <div className="w-full space-y-6">
+        {/* Statsエリア */}
+        <div className="p-5 border rounded-md">
+          <h3 className="text-lg font-medium mb-4">Stats</h3>
+          <div className="grid grid-cols-6 gap-4">
+            {/* 項目名 */}
+            <div className="bg-blue-700 text-white text-center">G</div>
+            <div className="bg-blue-700 text-white text-center">GS</div>
+            <div className="bg-blue-700 text-white text-center">MPG</div>
+            <div className="bg-blue-700 text-white text-center">PPG</div>
+            <div className="bg-blue-700 text-white text-center">RPG</div>
+            <div className="bg-blue-700 text-white text-center">APG</div>
+            {/* 値 */}
+            <div className="text-black text-center text-xl">{player.game}</div>
+            <div className="text-black text-center text-xl">{player.gs}</div>
+            <div className="text-black text-center text-xl">{player.mpg}</div>
+            <div className="text-black text-center text-xl">{player.ppg}</div>
+            <div className="text-black text-center text-xl">{player.rpg}</div>
+            <div className="text-black text-center text-xl">{player.apg}</div>
+            {/* 項目名 */}
+            <div className="bg-blue-700 text-white text-center">SPG</div>
+            <div className="bg-blue-700 text-white text-center">BPG</div>
+            <div className="bg-blue-700 text-white text-center">TO</div>
+            <div className="bg-blue-700 text-white text-center">FG%</div>
+            <div className="bg-blue-700 text-white text-center">3P%</div>
+            <div className="bg-blue-700 text-white text-center">FT%</div>
+            {/* 値 */}
+            <div className="text-black text-center text-xl">{player.spg}</div>
+            <div className="text-black text-center text-xl">{player.bpg}</div>
+            <div className="text-black text-center text-xl">
+              {player.turnover}
+            </div>
+            <div className="text-black text-center text-xl">
+              {player.percent_fg}%
+            </div>
+            <div className="text-black text-center text-xl">
+              {player.percent_3point}%
+            </div>
+            <div className="text-black text-center text-xl">
+              {player.percent_ft}%
+            </div>
           </div>
         </div>
 
-        <div className="w-full p-5 border rounded-md">
-          <input
-            className="w-2/3 p-2 border rounded-md"
-            type="text"
-            value={answer}
-            onChange={(e) => setAnswer(e.target.value)}
-            placeholder="選手の名前を入力"
-          />
-          <button
-            className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-md"
-            onClick={checkAnswer}
-          >
-            Answer
-          </button>
-          <div className="mt-4">{result}</div>
+        {/* ヒントゾーンと回答フォーム */}
+        <div className="w-full flex">
+          {/* ヒントゾーン */}
+          <div className="flex-1 p-4 border rounded-md">
+            <h3 className="text-lg font-medium mb-4">ヒント</h3>
+            <button
+              className={`px-4 py-4 mb-6 block w-full ${
+                hintsShown.team
+                  ? "bg-gray-300 cursor-default"
+                  : "bg-blue-500 text-white"
+              } rounded-md`}
+              onClick={() => showHint("team")}
+              disabled={hintsShown.team}
+            >
+              チーム:
+              {hintsShown.team &&
+                `${player.team}${player.team2 ? "-" + player.team2 : ""}`}
+            </button>
+            <button
+              className={`px-4 py-4 mb-6 block w-full ${
+                hintsShown.position
+                  ? "bg-gray-300 cursor-default"
+                  : "bg-blue-500 text-white"
+              } rounded-md`}
+              onClick={() => showHint("position")}
+              disabled={hintsShown.position}
+            >
+              ポジション:
+              {hintsShown.position &&
+                `${player.position}${
+                  player.position2 ? "/" + player.position2 : ""
+                }`}
+            </button>
+            <button
+              className={`px-4 py-4 block w-full ${
+                hintsShown.size
+                  ? "bg-gray-300 cursor-default"
+                  : "bg-blue-500 text-white"
+              } rounded-md`}
+              onClick={() => showHint("size")}
+              disabled={hintsShown.size}
+            >
+              体重/身長:
+              {hintsShown.size && `${player.kg}/${player.meter}`}
+            </button>
+          </div>
+
+          {/* 回答フォーム */}
+          <div className="flex-1 ml-4 p-5 border rounded-md flex flex-col justify-center items-center">
+            <input
+              className="w-2/3 p-2 border rounded-md mb-4"
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              placeholder="選手の名前を入力"
+            />
+            <button
+              className="ml-4 w-full px-4 py-4 bg-green-500 text-white rounded-md"
+              onClick={checkAnswer}
+            >
+              Answer
+            </button>
+            <div className="mt-4 text-xl">{result}</div>
+          </div>
         </div>
       </div>
     </div>
